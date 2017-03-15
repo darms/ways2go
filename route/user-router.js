54a4ebabeca9732eab713a85adc65f0107c98271
darms <darmcca@gmail.com>
2017-03-14 18:51:15 -0700

    Be nice, Travis!

--- route/user-router.js
+++ route/user-router.js
@@ -4,6 +4,7 @@ const debug = require('debug')('ways2go:user-router');
 const jsonParser = require('body-parser').json();
 const Router = require('express').Router;
 const basicAuth = require('../lib/basic-auth-middleware.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const createError = require('http-errors');
 
 const User = require('../model/user.js');
@@ -27,13 +28,49 @@ userRouter.post('/api/user', jsonParser, function(req, res, next) {
 
 userRouter.get('/api/user', basicAuth, function(req, res, next) {
   debug('GET: /api/user');
-
   User.findOne({ username: req.auth.username })
   .then( user => {
-    if (!user) return next(createError(401, 'username not found'));
+    if (!user) return next(createError(404, 'user not found'));
     return user.comparePasswordHash(req.auth.password);
   })
   .then( user => user.generateToken())
   .then( token => res.send(token))
+  .catch(next);
+});
+
+userRouter.put('/api/user', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT /api/user');
+
+  User.findById(req.user._id)
+  .then( user => {
+    if (!user) return next(createError(404, 'user not found'));
+    if (user._id.toString() !== req.user._id.toString()) {
+      return next(createError(401, 'Unauthorized User'));
+    }
+    return User.findByIdAndUpdate(req.user._id, req.body, { new: true });
+  })
+  .then( user => {
+    let reqKeys = Object.keys(req.body);
+    if (!user[reqKeys]) return next(createError(400, 'bad request'));
+    res.json(user);
+  })
+  .catch(next);
+});
+
+userRouter.delete('/api/user', bearerAuth, function(req, res, next) {
+  debug('DELETE: /api/user');
+
+  User.findById(req.user._id)
+  .then( user => {
+    if (!user) return next(createError(404, 'user not found'));
+    if (user._id.toString() !== req.user._id.toString()) {
+      return next(createError(401, 'Unauthorized User'));
+    }
+    return User.findByIdAndRemove(req.user._id);
+  })
+  .then( removed => {
+    if (!removed) return next(createError(404, 'user not found'));
+    return next(res.status(204).send('account removed'));
+  })
   .catch(next);
 });
--- test/user-router-test.js
+++ test/user-router-test.js
@@ -38,10 +38,38 @@ describe('User Routes', function() {
         });
       });
     });
+
+    describe('without a request body', () => {
+      it('should return a 400 error', done => {
+        request.post(`${url}/api/user`)
+        .end((err, res) => {
+          console.log('res', res.text);
+          expect(err.status).to.equal(400);
+          expect(res.status).to.equal(err.status);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid email', () => {
+      it('should return a 400 error', done => {
+        request.post(`${url}/api/user`)
+        .send({
+          username: 'test',
+          password: 'checkit',
+          email: 'notanemail'
+        })
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          expect(res.status).to.equal(err.status);
+          done();
+        });
+      });
+    });
   });
 
   describe('GET: /api/user', function() {
-    before( done => {
+    beforeEach( done => {
       let user = new User(testUser);
       user.generatePasswordHash(user.password)
       .then( user => user.save())
@@ -60,6 +88,187 @@ describe('User Routes', function() {
           if (err) return done(err);
           expect(res.status).to.equal(200);
           expect(res.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+
+    describe('without a request body', () => {
+      it('should return a 401 error', done => {
+        request.get(`${url}/api/user`)
+        .end((err, res) => {
+          expect(err.status).to.equal(401);
+          expect(res.text).to.equal('UnauthorizedError');
+          done();
+        });
+      });
+    });
+
+    describe('without a username', () => {
+      it('should return a 401 error', done => {
+        request.get(`${url}/api/user`)
+        .auth('', 'password')
+        .end((err, res) => {
+          expect(err.status).to.equal(401);
+          expect(res.text).to.equal('UnauthorizedError');
+          expect(err.message).to.equal('Unauthorized');
+          done();
+        });
+      });
+    });
+
+    describe('without a password', () => {
+      it('should return a 401 error', done => {
+        request.get(`${url}/api/user`)
+        .auth('tester name', '')
+        .end((err, res) => {
+          expect(err.status).to.equal(401);
+          expect(res.text).to.equal('UnauthorizedError');
+          expect(err.message).to.equal('Unauthorized');
+          done();
+        });
+      });
+    });
+
+    describe('with an unrecognized user', () => {
+      it('should return a 404 error', done => {
+        request.get(`${url}/api/user`)
+        .auth('not the name', 'password')
+        .end((err, res) => {
+          expect(err.status).to.equal(404);
+          expect(res.text).to.equal('NotFoundError');
+          expect(err.message).to.equal('Not Found');
+          done();
+        });
+      });
+    });
+
+    describe('with an incorrect password', () => {
+      it('should return a 401 error', done => {
+        request.get(`${url}/api/user`)
+        .auth('tester name', 'wrong')
+        .end((err, res) => {
+          console.log('err', err.message);
+          console.log('res', res.text);
+          expect(err.status).to.equal(401);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('PUT: /api/user', function() {
+    beforeEach( done => {
+      let user = new User(testUser);
+      user.generatePasswordHash(user.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    describe('with a valid request', () => {
+      it('should return an updated user', done => {
+        request.put(`${url}/api/user`)
+        .send({ email: 'new@email.com' })
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.email).to.equal('new@email.com');
+          done();
+        });
+      });
+    });
+
+    describe('without a request body', () => {
+      it('should return a 400 error', done => {
+        request.put(`${url}/api/user`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          expect(err.message).to.equal('Bad Request');
+          expect(res.text).to.equal('BadRequestError');
+          done();
+        });
+      });
+    });
+
+    describe('without a token', () => {
+      it('should return a 401 error', done => {
+        request.put(`${url}/api/user`)
+        .send({ email: 'new@email.com'})
+        .end((err, res) => {
+          expect(err.status).to.equal(401);
+          expect(err.message).to.equal('Unauthorized');
+          expect(res.text).to.equal('UnauthorizedError');
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid request', () => {
+      it('should return a 400 error', done => {
+        request.put(`${url}/api/user`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .send({ favColor: 'blue' })
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          console.log('res', res.text);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('DELETE: /api/user', function() {
+    beforeEach( done => {
+      let user = new User(testUser);
+      user.generatePasswordHash(testUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    describe('with a valid request', () => {
+      it('should return a 204 status code', done => {
+        request.delete(`${url}/api/user`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+      });
+    });
+
+    describe('without a token', () => {
+      it('should return a 401 error', done => {
+        request.delete(`${url}/api/user`)
+        .end((err, res) => {
+          expect(err.status).to.equal(401);
+          expect(res.text).to.equal('UnauthorizedError');
           done();
         });
       });
